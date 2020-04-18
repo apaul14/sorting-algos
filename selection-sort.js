f9391108779d6a00fa0898f3f2459fdb931092ab
Alex Paul <al.9thousand@gmail.com>
2020-04-17 22:14:42 -0400

    selection sort v1

--- selection-sort.js
+++ selection-sort.js
@@ -1,23 +1,17 @@
 const selectionSort = arr => {
-  const swapHelper = (arr, i, j, min) => {
-    let temp = arr[i]
-    arr[i] = min
-    arr[j] = temp
+  const swapHelper = (arr, a, b) => {
+    let temp = arr[b]
+    arr[b] = arr[a]
+    arr[a] = temp
   }
-
-  for (let i = 0; i < arr.length; i++) {
-    let min = arr[i]
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < min) {
-        min = arr[j]
-      }
-      if (min !== arr[i]) {
-        swapHelper(arr, i, j, min)
-        min = arr[i]
-      }
-    }
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        swapHelper(arr, i, j)
+     }
+   }
   }
-  return arr
+return arr
 }
 
 console.log(selectionSort([3,5,7,7,45,345,234,356,345,6345,5,6,7,2,2,5,6,7]))
