ebf89a874f8a7fb600a42dfbc4f2e31c3241a234
Alex Paul <al.9thousand@gmail.com>
2020-04-19 14:23:37 -0400

    insertion sort v2

---
+++ insertion-sort.js
@@ -0,0 +1,22 @@
+//first pass, not exactley correct sorting process
+
+const insertionSort = arr => {
+  const swapHelper = (arr, a, b) => {
+    let temp = arr[b]
+    arr[b] = arr[a]
+    arr[a] = temp
+  }
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        swapHelper(arr, i, j)
+     }
+   }
+  }
+return arr
+}
+
+insertionSort([423,1,5,6,3,4345,456,43,525,348,4,78,546,2346,3568])
+
+//or
+
