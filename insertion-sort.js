ced47aa4f36ecc15625b37a6b6f76f1c6fc4ba33
Alex Paul <al.9thousand@gmail.com>
2020-04-20 22:55:12 -0400

    insertion sort v2

--- insertion-sort.js
+++ insertion-sort.js
@@ -20,3 +20,19 @@ console.log(insertionSort([423,1,5,6,3,4345,456,43,525,348,4,78,546,2346,3568]))
 
 //or
 
+const insertionSort2 = arr => {
+  let length = arr.length;
+    for (let i = 1; i < length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 &&arr[j] > key) {
+           arr[j + 1] =arr[j];
+            j = j - 1;
+        }
+       arr[j + 1] = key;
+    }
+    return arr;
+}
+
+console.log(insertionSort2([423,1,5,6,3,4345,456,43,525,348,4,78,546,2346,3568]))
+
